eca981eaedcb45c584a846908e199932ae0b855f
Songheng <yinsongheng@gmail.com>
2020-12-08 15:03:56 -0500

    PartA Q1 ver1.0 done

--- Final Project/starter_code/part_a/knn.py
+++ Final Project/starter_code/part_a/knn.py
@@ -3,7 +3,6 @@ sys.path.append('..')
 from sklearn.impute import KNNImputer
 from utils import *
 from matplotlib import pyplot as plt
-# from sklearn.neighbors import KNeighborsClassifier
 
 def knn_impute_by_user(matrix, valid_data, k, print_option=True):
     """ Fill in the missing values using k-Nearest Neighbors based on
@@ -27,7 +26,7 @@ def knn_impute_by_user(matrix, valid_data, k, print_option=True):
     return acc
 
 
-def knn_impute_by_item(matrix, valid_data, k):
+def knn_impute_by_item(matrix, valid_data, k, print_option=True):
     """ Fill in the missing values using k-Nearest Neighbors based on
     question similarity. Return the accuracy on valid_data.
 
@@ -41,7 +40,12 @@ def knn_impute_by_item(matrix, valid_data, k):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    acc = None
+    nbrs = KNNImputer(n_neighbors=k)
+    # We use NaN-Euclidean distance measure.
+    mat = nbrs.fit_transform(matrix.T)
+    acc = sparse_matrix_evaluate(valid_data, mat.T)
+    if print_option:
+        print("Validation Accuracy: {}".format(acc))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -53,12 +57,10 @@ def main():
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
 
-    print("Sparse matrix:")
-    print(sparse_matrix)
-    print("Shape of sparse matrix:")
-    print(sparse_matrix.shape)
-    # train_data = load_train_csv('../data')
-    # print(len(train_data['question_id']))
+    # print("Sparse matrix:")
+    # print(sparse_matrix)
+    # print("Shape of sparse matrix:")
+    # print(sparse_matrix.shape)
     #####################################################################
     # TODO:                                                             #
     # Compute the validation accuracy for each k. Then pick k* with     #
@@ -76,11 +78,24 @@ def main():
     ax.set_title('PartA 1(a)')
     plt.savefig('./1(a).png')
 
-    # Question 2(a)
+    # Question 1(b)
     best_k_user = k_values[np.argmax(acc_list_user)]
     test_acc_user = knn_impute_by_user(sparse_matrix, test_data, best_k_user, print_option=False)
     print('User-based: Chosen k = {}, Test accuracy = {}.'.format(best_k_user, test_acc_user))
 
+    # Question 1(c)
+    acc_list_item = []
+    for k in k_values:
+        acc_list_item.append(knn_impute_by_item(sparse_matrix, val_data, k))
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(k_values, acc_list_item)
+    ax.set_xlabel('K value')
+    ax.set_ylabel('Accuracy on validation data')
+    ax.set_title('PartA 1(c)')
+    plt.savefig('./1(c).png')
+    best_k_item = k_values[np.argmax(acc_list_item)]
+    test_acc_item = knn_impute_by_user(sparse_matrix, test_data, best_k_item, print_option=False)
+    print('Item-based: Chosen k = {}, Test accuracy = {}.'.format(best_k_item, test_acc_item))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
